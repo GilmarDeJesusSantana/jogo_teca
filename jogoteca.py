9730f1bef8843f698c69376fd2ca56f5034b1c23
Gilmar <gilmar.jesus@gmail.com>
2022-05-04 20:36:21 -0300

    Adicionado lista de jogos usando variaveis.

--- jogoteca.py
+++ jogoteca.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    lista_jogos = ['Tetris', 'Skyrim', 'Crash']
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+
 
 app.run()
