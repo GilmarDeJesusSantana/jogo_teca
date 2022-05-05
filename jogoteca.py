cc99aaded6c65f54dbb95ecdc71a0973dc0f638e
Gilmar <gilmar.jesus@gmail.com>
2022-05-05 17:06:10 -0300

    Implementado a funcionalidade de logout.

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 from jogos import Jogos
 
@@ -9,6 +9,7 @@ jogo2 = Jogos('Good of War', 'Rack n Slash', 'PS2')
 jogo3 = Jogos('Mortal Kombat', 'Luta', 'Snes')
 lista_jogos = [jogo1, jogo2, jogo3]
 
+
 @app.route('/')
 def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
@@ -16,10 +17,11 @@ def index():
 
 @app.route('/cadastro-de-jogos')
 def cadastro_de_jogos():
-    nome_formulario='Cadastro de Jogos'
+    nome_formulario = 'Cadastro de Jogos'
     return render_template('cadastro_de_jogos.html', titulo=nome_formulario)
 
-@app.route('/criar', methods=['POST',])
+
+@app.route('/criar', methods=['POST', ])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
@@ -33,11 +35,24 @@ def criar():
 def login():
     return render_template('login.html')
 
+
 @app.route('/autenticar', methods=['Post'])
 def autenticar():
     if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso.')
         return redirect('/')
     else:
+        flash('Usuário não logado.')
         return redirect('/login')
 
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado!')
+    return redirect('/')
+
+
+app.secret_key = 'Gilmar'
 app.run(debug=True)
