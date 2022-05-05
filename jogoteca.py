949e51b8c8d15c072ab596c8def057b462a53952
Gilmar <gilmar.jesus@gmail.com>
2022-05-04 21:34:57 -0300

    Permitido o metodo POST na rota criar.

--- jogoteca.py
+++ jogoteca.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from jogos import Jogos
 
 app = Flask(__name__)
 
+jogo1 = Jogos('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogos('Good of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogos('Mortal Kombat', 'Luta', 'Snes')
+lista_jogos = [jogo1, jogo2, jogo3]
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogos('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogos('Good of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogos('Mortal Kombat', 'Luta', 'Snes')
-    lista_jogos = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
 
@@ -18,4 +18,13 @@ def ola():
 def cadastro_de_jogos():
     nome_formulario='Cadastro de Jogos'
     return render_template('cadastro_de_jogos.html', titulo=nome_formulario)
-app.run()
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogos(nome, categoria, console)
+    lista_jogos.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos= lista_jogos)
+app.run(debug=True)
